f2c998034eb15bb208926d254adb5721e8155b4e
Ben McLean <35312653+BenMcLean981@users.noreply.github.com>
2024-08-10 01:22:42 -0400

    Added TsConfig structure.

--- src/tsconfig/spec/ts-config.spec.ts
+++ src/tsconfig/spec/ts-config.spec.ts
@@ -1,9 +1,42 @@
-import { describe, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
+import { TsConfig } from '../ts-config';
+import { Reference } from '../reference';
 
 describe('TsConfig', () => {
-  describe("equals", () => {
-    it("Returns true for equal.", () => {
-      
-    })
-  })
+  describe('equals', () => {
+    it('Returns true for equal.', () => {
+      const t1 = new TsConfig({
+        references: [new Reference('../foo')],
+      });
+      const t2 = new TsConfig({
+        references: [new Reference('../foo')],
+      });
+
+      expect(t1.equals(t2)).toBe(true);
+    });
+
+    it('Returns false for different references.', () => {
+      const t1 = new TsConfig({
+        references: [new Reference('../foo')],
+      });
+      const t2 = new TsConfig({
+        references: [new Reference('../bar')],
+      });
+
+      expect(t1.equals(t2)).toBe(false);
+    });
+
+    it('Returns false for different additionalData.', () => {
+      const t1 = new TsConfig({
+        references: [new Reference('../foo')],
+        additionalData: { compilerOptions: { strict: true } },
+      });
+      const t2 = new TsConfig({
+        references: [new Reference('../foo')],
+        additionalData: { compilerOptions: { strict: false } },
+      });
+
+      expect(t1.equals(t2)).toBe(false);
+    });
+  });
 });
--- src/tsconfig/ts-config.ts
+++ src/tsconfig/ts-config.ts
@@ -1,1 +1,30 @@
-export class TsConfig {}
+import { isEqual } from 'lodash';
+import { haveSameItems } from '../haveSameItems';
+import { Snapshot } from '../snapshot';
+import { Reference } from './reference';
+
+export class TsConfig {
+  private readonly _references: ReadonlyArray<Reference>;
+
+  private readonly _additionalData: Snapshot;
+
+  public constructor(args?: {
+    references?: ReadonlyArray<Reference>;
+    additionalData?: Snapshot;
+  }) {
+    this._references = args?.references ?? [];
+    this._additionalData = args?.additionalData ?? {};
+  }
+
+  public equals(other: TsConfig): boolean {
+    const sameReferences = haveSameItems(
+      this._references,
+      other._references,
+      (r1, r2) => r1.equals(r2),
+    );
+
+    return (
+      sameReferences && isEqual(this._additionalData, other._additionalData)
+    );
+  }
+}
