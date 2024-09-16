2c9749ea17d60588df765991721b938565efa3ff
Ben McLean <35312653+BenMcLean981@users.noreply.github.com>
2024-09-16 14:06:54 -0400

    Fixed build.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,31 @@
-import { resolve } from 'node:path';
+import { glob } from 'glob';
+import { extname, relative, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    copyPublicDir: false,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'monorepo-utils',
-      formats: ['es', 'cjs'],
-      fileName: (format) => `index.${format}.js`,
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime'],
+      input: Object.fromEntries(
+        glob
+          .sync('src/**/*.ts', { ignore: ['src/**/*.d.ts'] })
+          .map((file) => [
+            relative('src', file.slice(0, file.length - extname(file).length)),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ]),
+      ),
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
+      },
     },
   },
   resolve: {
